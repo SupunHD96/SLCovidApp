daef85627e40f903896ecb6cf6f7ceaedeeb9783
KisalRojitha <72309530+KisalRojitha@users.noreply.github.com>
2020-11-24 21:05:27 +0530

    added Footer

--- client/my-app/src/components/Footer/Footer.jsx
+++ client/my-app/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(){
         <footer className="page-footer teal lighten-2 center">
          
             <div className="row">
-                <div className="col s10"> <h5 className="black-text">All Rights Reserved</h5></div>
+                <div className="col s12"> <h5 className="black-text">All Rights Reserved</h5></div>
                 <div className="col s12"><p className="black-text">&#169; Coded_life</p></div>
             </div>
         </footer>
