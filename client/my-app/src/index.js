020efc9170263a236b9a8648bb22be2c439c2f10
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2020-12-01 16:18:24 +0530

    PWA test-2

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from "./serviceWorker";
 import 'materialize-css/dist/css/materialize.min.css'
 
@@ -17,4 +16,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 serviceWorker.register();
-reportWebVitals();
