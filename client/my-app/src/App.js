bb81024f8e97ea11475751f336998c89ca180786
SupunHD96 <SupunHD96@users.noreply.github.com>
2020-12-01 22:12:34 +0530

    Some covid prevention details added

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import Landing from "./components/Landing";
 import Navigation from "./components/Navigation/Navigation";
 import Footer from "./components/Footer/Footer";
@@ -6,9 +7,11 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
           <Navigation />
           <Landing />
           <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
--- client/my-app/src/components/Navigation/Navigation.js
+++ client/my-app/src/components/Navigation/Navigation.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import { NavHashLink } from 'react-router-hash-link';
 import './Navigation.css';
 
 export default function Navigation(){
 
         return (
-          <div>
-            <div className="navbar-fixed ">
-              <nav>
-                <div className="nav-wrapper"  style={{backgroundColor:"#1FA2FF"}}>
-                  <a  href='#intro'>Home</a>
-                  <ul id="nav-large" className="right hide-on-small-only">
-                    <li><a  href='#local'>Local Data</a></li>
-                    <li><a  href='#global'>Global Data</a></li>
-                    <li><a  href='#safety'>Prevention</a></li>
-                    <li></li>
-                  </ul>
+          <div className="navbar-fixed ">
+          <nav>
+            <div className="nav-wrapper"  style={{backgroundColor:"#1FA2FF"}}>
+              <li className="brand-logo"><NavHashLink smooth to='/landing#intro'>Home</NavHashLink></li>
+              <ul id="nav-large" className="right hide-on-small-only">
+                <li><NavHashLink smooth to='/landing#local'>Local Data</NavHashLink></li>
+                <li><NavHashLink smooth to='/landing#global'>Global Data</NavHashLink></li>
+                <li><NavHashLink smooth to='/safety#safety'>Prevention</NavHashLink></li>
+                <li></li>
+              </ul>
 
-                  <div>
-                  <ul id="nav-mobile" className="hide-on-med-and-up">
-                    <li><a href='#intro'>HomeMob</a></li>
-                    <li><a href='#safety'>PreventionMob</a></li>
-                    <li></li>
-                  </ul>
-                  </div>
-                  
-                </div>
-              </nav>
+              <div>
+              <ul id="nav-mobile" className="hide-on-med-and-up">
+                <li><NavHashLink to='/landing#intro'>HomeMob</NavHashLink></li>
+                <li><NavHashLink to='/safety#safety'>PreventionMob</NavHashLink></li>
+                <li></li>
+              </ul>
+              </div>
+
+
             </div>
-          </div>
+          </nav>
+        </div>
         );
     }
