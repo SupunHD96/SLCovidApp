773c511f9ec9cb93e9780512d71ca3f82c2dfbb9
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2020-12-01 19:02:48 +0530

    fixed issue in NavHashLink

--- client/my-app/src/components/Navigation/Navigation.js
+++ client/my-app/src/components/Navigation/Navigation.js
@@ -9,7 +9,7 @@ export default function Navigation(){
             <div className="navbar-fixed ">
               <nav>
                 <div className="nav-wrapper"  style={{backgroundColor:"#1FA2FF"}}>
-                  <a className="brand-logo"><NavHashLink smooth to='/landing#intro'>Home</NavHashLink></a>
+                  <NavHashLink smooth to='/landing#intro'>Home</NavHashLink>
                   <ul id="nav-large" className="right hide-on-small-only">
                     <li><NavHashLink smooth to='/landing#local'>Local Data</NavHashLink></li>
                     <li><NavHashLink smooth to='/landing#global'>Global Data</NavHashLink></li>
