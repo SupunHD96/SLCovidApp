23a33a3cffa5a9fa1571462cf670ded4d329a2d2
SupunHD96 <SupunHD96@users.noreply.github.com>
2020-12-06 03:29:02 +0530

    Mobile navigation working (needs styling)

--- client/my-app/src/components/Navigation/Navigation.js
+++ client/my-app/src/components/Navigation/Navigation.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import { Component, React } from 'react';
 import { NavHashLink } from 'react-router-hash-link';
+import M from 'materialize-css';
 import './Navigation.css';
 
-export default function Navigation(){
 
-        return (
-          <div className="navbar-fixed ">
-          <nav>
-            <div className="nav-wrapper"  style={{backgroundColor:"#1FA2FF"}}>
-              <li className="brand-logo"><NavHashLink smooth to='/landing#intro'>Home</NavHashLink></li>
-              <ul id="nav-large" className="right hide-on-small-only">
-                <li><NavHashLink smooth to='/landing#local'>Local Data</NavHashLink></li>
-                <li><NavHashLink smooth to='/landing#global'>Global Data</NavHashLink></li>
-                <li><NavHashLink smooth to='/safety#safety'>Prevention</NavHashLink></li>
-                <li></li>
-              </ul>
+class Navigation extends Component {
 
-              <div>
-              <ul id="nav-mobile" className="hide-on-med-and-up">
-                <li><NavHashLink to='/landing#intro'>HomeMob</NavHashLink></li>
-                <li><NavHashLink to='/safety#safety'>PreventionMob</NavHashLink></li>
-                <li></li>
-              </ul>
-              </div>
+  componentDidMount() {
+    let elems = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elems, { inDuration: 300, outDuration: 225 });
+  }
 
+  render() {
+    return (
+      <div className="navbar-fixed ">
+        <nav>
+          <div className="nav-wrapper" style={{ backgroundColor: "#1FA2FF" }}>
+            <i className="brand-logo"><NavHashLink smooth to='/landing#intro'>Home</NavHashLink></i>
 
-            </div>
-          </nav>
-        </div>
-        );
-    }
+            <i className="material-icons dropdown-trigger btn-flat hide-on-med-and-up" data-target='nav-dropdown' style={{ backgroundColor: "#1FA2FF" }}>menu</i>
+            <ul id="nav-dropdown" className="dropdown-content hide-on-med-and-up">
+              <li className="collection-item"><NavHashLink smooth to='/landing#local'>Local Data</NavHashLink></li>
+              <li className="collection-item"><NavHashLink smooth to='/landing#global'>Global Data</NavHashLink></li>
+              <li className="collection-item"><NavHashLink smooth to='/safety#safety'>Prevention</NavHashLink></li>
+            </ul>
+
+            <ul id="nav-large" className="right hide-on-small-only">
+              <li><NavHashLink smooth to='/landing#local'>Local Data</NavHashLink></li>
+              <li><NavHashLink smooth to='/landing#global'>Global Data</NavHashLink></li>
+              <li><NavHashLink smooth to='/safety#safety'>Prevention</NavHashLink></li>
+            </ul>
+
+          </div>
+
+        </nav>
+      </div>
+
+    )
+  }
+}
+
+export default Navigation;
