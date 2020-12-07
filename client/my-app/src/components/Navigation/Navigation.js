d3ae07ed22644e024c691daf62ad4697de0fd57f
Ishanka D Senevirathne <33729848+IshankaDSenevirathne@users.noreply.github.com>
2020-12-07 16:02:55 +0530

    logos added

--- client/my-app/src/components/Navigation/Navigation.js
+++ client/my-app/src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import { Component, React } from 'react';
 import { NavHashLink } from 'react-router-hash-link';
 import M from 'materialize-css';
 import './Navigation.css';
-import Flag from '../../Images/flag.png'
+import Flag from '../../images/flag.png';
 
 class Navigation extends Component {
 
